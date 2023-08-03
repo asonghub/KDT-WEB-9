176ede33939afc33d587e647e74fc737c7e280dc
Asong Ma <woss3223@gmail.com>
2023-08-03 21:03:07 +0900

    study: ajax, axios, fetch

---
+++ nodejsAll/0802_form_dynamic/index.js
@@ -0,0 +1,47 @@
+//필요한 모듈
+const express = require('express');
+const app = express();
+const PORT  = 8080;
+
+//body-parser
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+//router
+app.get('/', (req,res) =>{
+    res.render('index')
+})
+app.get('/ajax',(req,res)=>{
+    console.log('back', req.query);
+    res.send(req.query); //보낸걸 다시 확인하는용으로 다시 보냄.
+})
+app.post('/ajax',(req,res)=>{
+    console.log('back', req.body);
+    res.send(req.body);
+})
+
+app.get('/axios',(req,res)=>{
+    console.log('back', req.query);
+    res.send(req.query);
+})
+app.post('/axios',(req,res)=>{
+    console.log('back', req.body);
+    res.send(req.body);
+})
+app.get('/fetch',(req,res)=>{
+    console.log('back', req.query);
+    res.send(req.query);
+})
+app.post('/fetch',(req,res)=>{
+    console.log('back', req.body);
+    res.send(req.body);
+})
+
+//server start
+app.listen(PORT, ()=>{
+    console.log(`http://localhost:${PORT}`)
+});
